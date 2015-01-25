b815608c09d24c8edf7dd3c6957c79d6896fb14d
Vasil Kyuchukov <2090759k>
2015-01-25 20:30:21 +0000

    Chapter 4 Compleated

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Rango says hey there world! <br/> <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("This tutorial has been	put together by	Vasil Kyuchukov, 2090759K <br/> <a href='/rango/'>Rango</a> ")
+    
